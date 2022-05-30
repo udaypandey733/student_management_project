ab1e9c434978e977b1dac177500183c5f8f8fb22
udaypandey733 <udaypandey733@gmail.com>
2022-05-30 11:28:40 +0530

    comit 21

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -3,9 +3,9 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-1
+
 class SessionYearModel(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.Year(primary_key=True)
     session_start_year = models.DateField()
     session_end_year = models.DateField()
     objects = models.Manager()
